5ab3ba42da57fddbc2b1cdbc9b98b1f1dd3b12d2
mikhail <michael.maidan1998@gmail.com>
2025-01-06 16:42:04 +0100

    feat: make all rest tasks

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,15 +1,11 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 // const { Node } = require('../extensions/list-tree.js');
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
-
-
-
-
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 
 class Node {
   constructor(data) {
@@ -21,16 +17,14 @@ class Node {
 
 class BinarySearchTree {
   constructor() {
-    this._root = null; // we'll store the reference to the tree's root here
+    this._root = null;
   }
 
   root() {
-    // return the root node of the tree
     return this._root;
   }
 
   add(data) {
-    // add node with data to the tree
     this._root = addWithin(this._root, data);
 
     function addWithin(node, value) {
@@ -38,16 +32,14 @@ class BinarySearchTree {
       if (!node) {
         return new Node(value);
       }
-      // if the value is already in the tree, do not add duplicates
+
       if (node.data === value) {
         return node;
       }
-      // if value is less, go to the left subtree
+
       if (value < node.data) {
         node.left = addWithin(node.left, value);
-      }
-      // if value is greater, go to the right subtree
-      else {
+      } else {
         node.right = addWithin(node.right, value);
       }
       return node;
@@ -55,7 +47,6 @@ class BinarySearchTree {
   }
 
   has(data) {
-    // returns true if node with the data exists in the tree, false otherwise
     return searchWithin(this._root, data);
 
     function searchWithin(node, value) {
@@ -63,28 +54,26 @@ class BinarySearchTree {
 
       if (node.data === value) return true;
 
-      return value < node.data 
-        ? searchWithin(node.left, value) 
+      return value < node.data
+        ? searchWithin(node.left, value)
         : searchWithin(node.right, value);
     }
   }
 
   find(data) {
-    // returns node with the data if it exists in the tree, or null otherwise
     return findWithin(this._root, data);
 
     function findWithin(node, value) {
       if (!node) return null;
       if (node.data === value) return node;
 
-      return value < node.data 
-        ? findWithin(node.left, value) 
+      return value < node.data
+        ? findWithin(node.left, value)
         : findWithin(node.right, value);
     }
   }
 
   remove(data) {
-    // removes node with the data from the tree if such node exists
     this._root = removeNode(this._root, data);
 
     function removeNode(node, value) {
@@ -98,34 +87,30 @@ class BinarySearchTree {
         node.right = removeNode(node.right, value);
         return node;
       } else {
-        // value === node.data => this is the node to remove
-
-        // no children
         if (!node.left && !node.right) {
           return null;
         }
-        // only right child
+
         if (!node.left) {
           return node.right;
         }
-        // only left child
+
         if (!node.right) {
           return node.left;
         }
-        // both children => replace this node with the minimal from the right subtree
+
         let minFromRight = node.right;
         while (minFromRight.left) {
           minFromRight = minFromRight.left;
         }
-        node.data = minFromRight.data; // copy value
-        node.right = removeNode(node.right, minFromRight.data); // remove duplicate
+        node.data = minFromRight.data;
+        node.right = removeNode(node.right, minFromRight.data);
         return node;
       }
     }
   }
 
   min() {
-    // returns minimal value stored in the tree (or null if the tree has no nodes)
     if (!this._root) return null;
 
     let node = this._root;
@@ -136,7 +121,6 @@ class BinarySearchTree {
   }
 
   max() {
-    // returns maximal value stored in the tree (or null if the tree has no nodes)
     if (!this._root) return null;
 
     let node = this._root;
@@ -148,5 +132,5 @@ class BinarySearchTree {
 }
 
 module.exports = {
-  BinarySearchTree
+  BinarySearchTree,
 };
